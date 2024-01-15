8ec49b18bede6c9fc2561eb0bf260c33ead4b2ee
Snay77 <77ethan.barlet@gmail.com>
2024-01-15 11:47:16 +0100

    diamants 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { myTasks } from './Data/data';
 
 function App() {
   let name = "ETHANNNNNNN"
+    
+  const listTaches = myTasks.map(tache=>
+    <li>{tache.name}</li>
+    )
+
+
   return (
     <div className="App">
       Hello {name} !!!!!!!!!!!!!!!!!!!!!!!!!!!
+      <ul>
+        {listTaches}
+      </ul>
     </div>
   );
 }
---
+++ src/Data/data.js
@@ -0,0 +1,7 @@
+const myTasks = [
+    {"id":"1","name":"Manger Haricots"},
+    {"id":"2","name":"Devoirs"},
+    {"id":"3","name":"Ranger les chausettes"}
+];
+
+export {myTasks};
