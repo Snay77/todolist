75ea0bc4ce9c1134884c6ccfce4540a1a79f42df
Snay77 <77ethan.barlet@gmail.com>
2024-01-22 09:35:35 +0100

    TodoList fini

--- src/App/index.js
+++ src/App/index.js
@@ -5,18 +5,12 @@ import TodoList from '../TodoList';
 
 function App() {
   let name = "ETHANNN" //Définition d'une variable
-    
-  const listTaches = myTasks.map(tache=>
-    <Task key={tache.id} {...tache}/> //Pour chaque tache (...), ça affiche ce qui est dans le composant Task
-  )
-
+  
   return (
     <div className="App">
       Hello {name} !!!
       <h2 className={styles.title}>Taches à réaliser</h2> {/* Définit le style sur title qui est dans la variable style qu'on importe */}
-      <ul>
-        {listTaches} {/* Affiche la variable listTaches */}
-      </ul>
+      <TodoList/>
     </div>
   );
 }
--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -1,11 +1,19 @@
 import styles from './TodoList.module.css';
 import { myTasks } from '../Data/data';
 import { useState } from 'react';
+import Task from '../Task';
 
-function TodoList (listTaches) {
+
+function TodoList () {
+
+    const listTaches = myTasks.map(tache=>
+        <Task key={tache.id} {...tache}/> //Pour chaque tache (...), ça affiche ce qui est dans le composant Task
+      )
 
     return (
-
+        <ul>
+            {listTaches} {/* Affiche la variable listTaches */}
+        </ul>
     )
 }
 
