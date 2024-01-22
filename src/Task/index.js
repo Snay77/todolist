42b7f2c67d11c0b2b8791fcbb240232bda4b2215
Snay77 <77ethan.barlet@gmail.com>
2024-01-22 09:14:35 +0100

    Barre

--- src/Task/index.js
+++ src/Task/index.js
@@ -1,14 +1,28 @@
 import styles from './Task.module.css';
 import { myTasks } from '../Data/data';
+import { useState } from 'react';
 
 function Task (props) {
+    const [barre, setBarre] = useState(false);
+
     function doubleClic(name) {
         console.log(name)
     }
 
+    function barrer(){
+        setBarre(!barre)
+    }
+
     console.log(props)
+
+
     return (
-        <li onDoubleClick={e=>doubleClic(props.name)}>{props.name}</li>
+        <li 
+            className={barre?styles.line:styles.visible}
+            onDoubleClick={e=>doubleClic(props.name)} 
+            onClick={barrer}>
+            {props.name}
+        </li>
     )
 }
 
