af3fda70ad2b38a4f3dc62ec74e4bdb792bf6ab2
Lou-Anne <saejinmx@gmail.com>
2024-01-22 09:26:48 +0100

    Code annoté

--- src/App/index.js
+++ src/App/index.js
@@ -3,21 +3,18 @@ import { myTasks } from '../Data/data';
 import Task from '../Task';
 
 function App() {
-  let name = "ETHANNNNNNN"
+  let name = "ETHANNN" //Définition d'une variable
     
   const listTaches = myTasks.map(tache=>
-    <Task key={tache.id} {...tache}/>
+    <Task key={tache.id} {...tache}/> //Pour chaque tache (...), ça affiche ce qui est dans le composant Task
   )
 
   return (
     <div className="App">
-      Hello {name} !!!!!!!!!!!!!!!!!!!!!!!!!!!
-      <h2 className={styles.title}>Taches à réaliser</h2>
+      Hello {name} !!!
+      <h2 className={styles.title}>Taches à réaliser</h2> {/* Définit le style sur title qui est dans la variable style qu'on importe */}
       <ul>
-        {listTaches}
-        {/* {myTasks.map(tache=>
-          <li>{tache.name}</li>
-          )} */}
+        {listTaches} {/* Affiche la variable listTaches */}
       </ul>
     </div>
   );
