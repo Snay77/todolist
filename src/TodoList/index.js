97192ea100fa2317645d4736c1f9369e2dfbd2f5
Lou-Anne <saejinmx@gmail.com>
2024-02-19 08:23:18 +0100

    Annotations

--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -7,12 +7,12 @@ import { addTask } from '../Data/data';
 
 
 function TodoList () {
-    const [name,setName] = useState("")
-    const [allTasks, setAllTasks] = useState(myTasks)
+    const [name,setName] = useState("") //On définit la variable d'état sur rien
+    const [allTasks, setAllTasks] = useState(myTasks) //On définit la variable d'état sur le tableau MyTasks
 
     const listTaches = allTasks.map(tache=>
         <Task key={tache.id} {...tache}/> //Pour chaque tache (...), ça affiche ce qui est dans le composant Task
-      )
+    )
 
     function changeVal(e){
         e.preventDefault() /* Évite que ça repasse directement sur du vide quand on submit */
